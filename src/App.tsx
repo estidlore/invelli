0fcb62477cb1c0626aad21c2e19e900bda4d65a5
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-08-21 18:45:18 -0500

    chore: add get-random-values
    - Install get-random-values to polyfill uuid
    - Import get-random-values and realm
    - Enable global realm flag to ensure realm import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import "react-native-get-random-values";
+
 import React from "react";
+import Realm from "realm";
 
 import { Screen } from "components";
 import { TabNav } from "navigation/TabNav";
 import { RealmProvider } from "utils/db";
+
+Realm.flags.THROW_ON_GLOBAL_REALM = true;
 
 const App = (): JSX.Element => {
   return (
