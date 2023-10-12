d2c8173d6bd420ca2f2e42ac094b4ed04c7a5885
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-10-11 21:55:34 -0500

    feat: handle form with yup & ruxi
    - Add yup itemSchema
    - Replace states & callbacks hooks by useForm

--- src/views/modals/ItemForm/index.tsx
+++ src/views/modals/ItemForm/index.tsx
@@ -1,12 +1,37 @@
 import React, { useCallback } from "react";
 import { View } from "react-native";
+import { useForm } from "ruxi";
+import { number, object, string } from "yup";
 
 import { BarcodeScanner, Button, Input, Modal } from "components";
-import { useRealm } from "utils/db";
-import { useField } from "utils/form";
 
 import { styles } from "./styles";
 import type { ItemFormProps } from "./types";
+
+const itemSchema = object({
+  brand: string(),
+  code: string(),
+  cost: number()
+    .transform((value, original) => (original === "" ? undefined : value))
+    .required()
+    .typeError("cost must be a number")
+    .positive()
+    .integer(),
+  name: string().required(),
+  price: number()
+    .transform((value, original) => (original === "" ? undefined : value))
+    .required()
+    .typeError("price must be a number")
+    .positive()
+    .integer(),
+  quantity: number()
+    .transform((value, original) => (original === "" ? undefined : value))
+    .required()
+    .typeError("quantity must be a number")
+    .positive()
+    .integer(),
+  unit: string(),
+});
 
 const ItemForm = ({
   item = {},
@@ -14,79 +39,65 @@ const ItemForm = ({
   onSave,
   visible,
 }: ItemFormProps): JSX.Element => {
-  const [brand, setBrand] = useField("string", item.brand);
-  const [code, setCode] = useField("string", item.code);
-  const [cost, setCost] = useField("number", item.cost);
-  const [name, setName] = useField("string", item.name);
-  const [price, setPrice] = useField("number", item.price);
-  const [quantity, setQuantity] = useField("number", item.quantity);
-  const [unit, setUnit] = useField("string", item.unit);
+  const { fields, reset, submit } = useForm({
+    initialValues: {
+      brand: item.brand ?? "",
+      code: item.code ?? "",
+      cost: item.cost?.toString() ?? "",
+      name: item.name ?? "",
+      price: item.price?.toString() ?? "",
+      quantity: item.quantity?.toString() ?? "",
+      unit: item.unit ?? "",
+    },
+    onSubmit: (values): void => {
+      onSave?.(values);
+      onClose?.();
+    },
+    parser: {
+      cost: parseInt,
+      price: parseInt,
+      quantity: parseInt,
+    },
+    validation: itemSchema,
+  });
 
-  const db = useRealm();
-  const handleSave = useCallback(() => {
-    onSave?.({
-      brand,
-      code,
-      cost: parseInt(cost),
-      name,
-      price: parseInt(price),
-      quantity: parseInt(quantity),
-      unit,
-    });
+  const handleClose = useCallback(() => {
     onClose?.();
-  }, [db, brand, code, cost, name, onClose, onSave, price, quantity, unit]);
+    reset();
+  }, [reset]);
 
   return (
-    <Modal onClose={onClose} title={"Agregar artículo"} visible={visible}>
+    <Modal onClose={handleClose} title={"Agregar artículo"} visible={visible}>
       <View style={[styles.row, styles.input]}>
-        <BarcodeScanner onScan={setCode} />
+        <BarcodeScanner onScan={fields.code.onChange} />
         <Input
-          onChange={setCode}
+          {...fields.code}
           placeholder={"Código"}
           style={styles.codeInput}
-          value={code}
         />
       </View>
+      <Input {...fields.brand} placeholder={"Marca"} style={styles.input} />
       <Input
-        onChange={setBrand}
-        placeholder={"Marca"}
-        style={styles.input}
-        value={brand}
-      />
-      <Input
-        onChange={setCost}
+        {...fields.cost}
         placeholder={"Costo"}
         style={styles.input}
         type={"numeric"}
-        value={cost}
       />
+      <Input {...fields.name} placeholder={"Nombre"} style={styles.input} />
       <Input
-        onChange={setName}
-        placeholder={"Nombre"}
-        style={styles.input}
-        value={name}
-      />
-      <Input
-        onChange={setPrice}
+        {...fields.price}
         placeholder={"Precio"}
         style={styles.input}
         type={"numeric"}
-        value={price}
       />
       <Input
-        onChange={setQuantity}
+        {...fields.quantity}
         placeholder={"Cantidad"}
         style={styles.input}
         type={"numeric"}
-        value={quantity}
       />
-      <Input
-        onChange={setUnit}
-        placeholder={"Unidad (g)"}
-        style={styles.input}
-        value={unit}
-      />
-      <Button icon={"check"} onPress={handleSave}>
+      <Input {...fields.unit} placeholder={"Unidad (g)"} style={styles.input} />
+      <Button icon={"check"} onPress={submit}>
         {"Guardar"}
       </Button>
     </Modal>
