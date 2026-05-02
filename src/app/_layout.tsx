32769b54c734d7770961690b4af797fd8b68539e
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2026-05-01 22:37:08 -0500

    setup eslint

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
+const RootLayout = (): React.JSX.Element => {
   return <Stack />;
-}
+};
+
+export default RootLayout;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,17 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+const Index = (): React.JSX.Element => {
   return (
     <View
       style={{
+        alignItems: "center",
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>{"Edit app/index.tsx to edit this screen."}</Text>
     </View>
   );
-}
+};
+
+export default Index;
