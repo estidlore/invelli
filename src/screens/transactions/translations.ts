33f59d27cbda5cb617504d42e2d536215978ab33
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2026-06-28 17:26:05 -0500

    Replace /screens with /transactions

---
+++ src/screens/transactions/translations.ts
@@ -0,0 +1,12 @@
+import { createTranslations } from "@/core/language";
+
+const translations = createTranslations({
+  ENG: {
+    transactions: "Transactions",
+  },
+  SPA: {
+    transactions: "Transacciones",
+  },
+});
+
+export { translations };
