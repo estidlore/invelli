66150abbb98fae55eed331d91b6006854938d277
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2026-08-17 16:37:55 -0500

    Add SegmentedControl component

---
+++ src/components/SegmentedControl/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  btn: {
+    flexGrow: 1,
+  },
+  label: {
+    marginBottom: 4,
+  },
+  row: {
+    alignItems: "stretch",
+    borderRadius: 8,
+    borderWidth: 1,
+    flexDirection: "row",
+  },
+});
+
+export { styles };
